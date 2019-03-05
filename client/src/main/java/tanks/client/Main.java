49a774c6220c90083babee073c4603dc45fba0d3
Magnus Muru <murumagnus@gmail.com>
2019-03-05 02:13:34 +0200

    Initial crude UI design.

--- client/src/main/java/tanks/client/Main.java
+++ client/src/main/java/tanks/client/Main.java
@@ -2,15 +2,26 @@ package tanks.client;
 
 import javafx.animation.AnimationTimer;
 import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundImage;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import tanks.client.models.TankBase;
 import tanks.client.models.TankLocal;
 import tanks.client.networking.Connection;
 import tanks.client.networking.TankManager;
+import tanks.client.menu.Title;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -26,7 +37,7 @@ import java.util.concurrent.TimeUnit;
 
 public class Main extends Application {
     private static int windowWidth = 800;
-    private static int windowHeight = 600;
+    private static int windowHeight = 450;
 
     private static Connection connection;
     public static TankManager tankManager;
@@ -40,50 +51,106 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) {
-        connection = new Connection("network");
-        tankManager = new TankManager();
         root = new Group();
 
         scene = new Scene(root, windowWidth, windowHeight);
-
-        primaryStage.setTitle( "The Game" );
-        primaryStage.setMaxHeight(windowHeight);
-        primaryStage.setMinHeight(windowHeight);
-        primaryStage.setMaxWidth(windowWidth);
-        primaryStage.setMinWidth(windowWidth);
+        double CANVAS_WIDTH = 800;
+        final double CANVAS_HEIGHT = 450;
         primaryStage.setResizable(false);
 
-        connection.start();
+        final Image titleScreen = new Image("/gui/menus/Title.png"); //title screen image
+        final Image playButton = new Image("/gui/menus/PlayTransparent.png"); //the play button image
+        final Image customizeButton = new Image("/gui/menus/Customize.png");
+        final Image helpButton = new Image("/gui/menus/Help.png");
 
-        Text frameRate = new Text(10, 50, "This is a test");
+        final ImageView flashScreen_node = new ImageView();
+        flashScreen_node.setImage(titleScreen); //set the image of the title screen
 
-        AnimationTimer animationTimer = new AnimationTimer() {
-            long lastTime;
+        final Button play_button = new Button();
+        final ImageView play_button_node = new ImageView();
+        final Button customize_button = new Button();
+        final ImageView customize_button_node = new ImageView();
+        final Button help_button = new Button();
+        final ImageView help_button_node = new ImageView();
 
-            @Override
-            public void start() {
-                super.start();
-                this.lastTime = System.nanoTime();
-            }
 
-            @Override
-            public void handle(long now) {
-                double deltaNanos = now - lastTime;
-                double deltaMillis = deltaNanos / 1000000;
+        play_button_node.setImage(playButton); //set the image of the play button
+        customize_button_node.setImage(customizeButton);
+        help_button_node.setImage(helpButton);
 
-                frameRate.setText(String.format("%1$.2f", 1000 / deltaMillis) + " fps");
+        play_button.setGraphic(play_button_node);
+        play_button.setBackground(new Background(new BackgroundImage(new Image("/gui/menus/PlayBackgroundLong.png"),
+                null, null, null, null)));
+        customize_button.setGraphic(customize_button_node);
+        customize_button.setBackground(new Background(new BackgroundImage(new Image("/gui/menus/PlayBackgroundLong.png"),
+                null, null, null, null)));
+        help_button.setGraphic(help_button_node);
+        help_button.setBackground(new Background(new BackgroundImage(new Image("/gui/menus/PlayBackgroundLong.png"),
+                null, null, null, null)));
 
-                for (TankBase tank : tankManager.getTanks())
-                    tank.render();
+        /*
+         * create the container of those buttons
+         */
+        final VBox buttonContainer = new VBox(5);
+        buttonContainer.setAlignment(Pos.TOP_CENTER);
+        Insets buttonContainerPadding = new Insets(250, 1, 1, 1);
+        buttonContainer.setPadding(buttonContainerPadding);
+        buttonContainer.getChildren().addAll(play_button, customize_button, help_button);
 
-                lastTime = now;
-            }
-        };
-        animationTimer.start();
+        primaryStage.setTitle("Jago Tanks!");
+        primaryStage.getIcons().add(titleScreen); //stage icon
 
-        root.getChildren().add(frameRate);
 
-        primaryStage.setScene(scene);
+        StackPane root = new StackPane();
+
+        root.getChildren().addAll(flashScreen_node, buttonContainer); //add the title screen and button container to the stackpane
+        Scene theScene = new Scene(root, CANVAS_WIDTH, CANVAS_HEIGHT, Color.BLACK);
+        primaryStage.setScene(theScene);
         primaryStage.show();
+
+        play_button.setOnAction(e -> {
+            connection = new Connection("network");
+            tankManager = new TankManager();
+            primaryStage.setTitle("The Game");
+            primaryStage.setMaxHeight(windowHeight);
+            primaryStage.setMinHeight(windowHeight);
+            primaryStage.setMaxWidth(windowWidth);
+            primaryStage.setMinWidth(windowWidth);
+            primaryStage.setResizable(false);
+
+            connection.start();
+
+            Text frameRate = new Text(10, 50, "This is a test");
+
+            AnimationTimer animationTimer = new AnimationTimer() {
+                long lastTime;
+
+                @Override
+                public void start() {
+                    super.start();
+                    this.lastTime = System.nanoTime();
+                }
+
+                @Override
+                public void handle(long now) {
+                    double deltaNanos = now - lastTime;
+                    double deltaMillis = deltaNanos / 1000000;
+
+                    frameRate.setText(String.format("%1$.2f", 1000 / deltaMillis) + " fps");
+
+                    for (TankBase tank : tankManager.getTanks())
+                        tank.render();
+
+                    lastTime = now;
+                }
+            };
+            animationTimer.start();
+
+            root.getChildren().add(frameRate);
+
+            primaryStage.setScene(scene);
+            primaryStage.show();
+
+        });
     }
 }
---
+++ client/src/main/java/tanks/client/menu/Title.java
@@ -0,0 +1,77 @@
+package tanks.client.menu;
+
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.BackgroundImage;
+import javafx.scene.layout.BackgroundSize;
+import javafx.scene.layout.CornerRadii;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+
+public class Title {
+
+    public Stage showTitle(Stage theStage) {
+
+        final double CANVAS_WIDTH = 800;
+        final double CANVAS_HEIGHT = 450;
+        theStage.setResizable(false);
+
+        final Image titleScreen = new Image("/gui/menus/Title.png"); //title screen image
+        final Image playButton = new Image("/gui/menus/PlayTransparent.png"); //the play button image
+        final Image customizeButton = new Image("/gui/menus/Customize.png");
+        final Image helpButton = new Image("/gui/menus/Help.png");
+
+        final ImageView flashScreen_node = new ImageView();
+        flashScreen_node.setImage(titleScreen); //set the image of the title screen
+
+        final Button play_button = new Button();
+        final ImageView play_button_node = new ImageView();
+        final Button customize_button = new Button();
+        final ImageView customize_button_node = new ImageView();
+        final Button help_button = new Button();
+        final ImageView help_button_node = new ImageView();
+
+
+        play_button_node.setImage(playButton); //set the image of the play button
+        customize_button_node.setImage(customizeButton);
+        help_button_node.setImage(helpButton);
+
+        play_button.setGraphic(play_button_node);
+        play_button.setBackground(new Background(new BackgroundImage(new Image("/gui/menus/PlayBackgroundLong.png"),
+                null, null, null, null)));
+        customize_button.setGraphic(customize_button_node);
+        customize_button.setBackground(new Background(new BackgroundImage(new Image("/gui/menus/PlayBackgroundLong.png"),
+                null, null, null, null)));
+        help_button.setGraphic(help_button_node);
+        help_button.setBackground(new Background(new BackgroundImage(new Image("/gui/menus/PlayBackgroundLong.png"),
+                null, null, null, null)));
+
+        /*
+         * create the container of those buttons
+         */
+        final VBox buttonContainer = new VBox(5);
+        buttonContainer.setAlignment(Pos.TOP_CENTER);
+        Insets buttonContainerPadding = new Insets(250, 1, 1, 1);
+        buttonContainer.setPadding(buttonContainerPadding);
+        buttonContainer.getChildren().addAll(play_button, customize_button, help_button);
+
+        theStage.setTitle("Jago Tanks!");
+        theStage.getIcons().add(titleScreen); //stage icon
+
+        StackPane root = new StackPane();
+
+        root.getChildren().addAll(flashScreen_node, buttonContainer); //add the title screen and button container to the stackpane
+        Scene theScene = new Scene(root, CANVAS_WIDTH, CANVAS_HEIGHT, Color.BLACK);
+        theStage.setScene(theScene);
+        return theStage;
+    }
+}
